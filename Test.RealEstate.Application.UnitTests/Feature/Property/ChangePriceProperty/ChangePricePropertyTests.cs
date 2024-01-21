cb19bb02f9de0c7bc1d4cc52f6a31d74e68a101f
Hawin Caraballo <hawin.caraballo@softwareone.com>
2024-01-20 21:30:11 -0500

    Test Unitario change price

--- Test.RealEstate.Application.UnitTests/Feature/Property/ChangePriceProperty/ChangePricePropertyTests.cs
+++ Test.RealEstate.Application.UnitTests/Feature/Property/ChangePriceProperty/ChangePricePropertyTests.cs
@@ -2,8 +2,6 @@
 namespace Test.RealEstate.Application.UnitTests.Feature.Property.ChangePriceProperty
 {
     using AutoMapper;
-    using FluentValidation;
-    using Microsoft.EntityFrameworkCore.ChangeTracking;
     using Microsoft.Extensions.Logging;
     using Moq;
     using NUnit.Framework;
@@ -21,6 +19,7 @@ namespace Test.RealEstate.Application.UnitTests.Feature.Property.ChangePriceProp
         private Mock<IPropertyRepository> repositoryMock;
         private Mock<IMapper> mapperMock;
         private Mock<ILogger<ChangePricePropertyCommandHandler>> loggerMock;
+        private ChangePricePropertyCommand command;
 
         [SetUp]
         public void SetUp()
@@ -29,22 +28,22 @@ namespace Test.RealEstate.Application.UnitTests.Feature.Property.ChangePriceProp
             mapperMock = new Mock<IMapper>();
             loggerMock = new Mock<ILogger<ChangePricePropertyCommandHandler>>();
             handler = new ChangePricePropertyCommandHandler(repositoryMock.Object, mapperMock.Object, loggerMock.Object);
+
+            command = new ChangePricePropertyCommand
+            {
+                IdProperty = 1,
+                Price = 100
+            };
         }
 
         [Test]
         public async Task Handle_ValidCommand_ReturnsSuccessResponse()
         {
             // Arrange
-            var command = new ChangePricePropertyCommand
-            {
-                IdProperty = 1, 
-                Price = 100
-            };
-
             var existingProperty = new Property
             {
                 IdProperty = command.IdProperty,
-                Price = 50
+                Price = command.Price,
             };
 
             repositoryMock.Setup(repo => repo.GetByIdAsync(command.IdProperty))
@@ -54,7 +53,7 @@ namespace Test.RealEstate.Application.UnitTests.Feature.Property.ChangePriceProp
                 .ReturnsAsync(existingProperty); 
 
             mapperMock.Setup(mapper => mapper.Map<PropertyDto>(It.IsAny<Property>()))
-                .Returns(new PropertyDto() { IdProperty = 1, Name = "testName", Address = "tesAdd", IdOwner = 1, Price = 20 });
+                .Returns(new PropertyDto() { IdProperty = command.IdProperty, Name = "testName", Address = "tesAdd", IdOwner = 1, Price = command.Price });
 
             // Act
             var result = await handler.Handle(command, CancellationToken.None);
@@ -68,16 +67,6 @@ namespace Test.RealEstate.Application.UnitTests.Feature.Property.ChangePriceProp
         [Test]
         public async Task Handle_NonExistentProperty_ReturnsBadRequest()
         {
-            // Arrange
-            var command = new ChangePricePropertyCommand
-            {
-                IdProperty = 2, 
-                Price = 100
-            };
-
-            repositoryMock.Setup(repo => repo.GetByIdAsync(command.IdProperty))
-                .ReturnsAsync((new Property { IdProperty = 1, Name = "testName", Address = "tesAdd", IdOwner = 1, Price = 20 })); 
-
             // Act
             var result = await handler.Handle(command, CancellationToken.None);
 
@@ -85,6 +74,18 @@ namespace Test.RealEstate.Application.UnitTests.Feature.Property.ChangePriceProp
             Assert.That(result, Is.Not.Null);
             Assert.That(result.Status, Is.False);
             Assert.That(result.Message, Is.EqualTo(ConstantPropertyText.PropertyNoExists));
+        }
+
+        [Test]
+        public async Task Handle_ErrorUpdateProperty_ReturnsBadRequest()
+        {
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Status, Is.False);
+            Assert.That(result.Message, Is.EqualTo(ConstantPropertyText.ErrorUpdatePropertyPrice));
         }
 
     }
