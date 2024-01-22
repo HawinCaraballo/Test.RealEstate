8a52bd05c83f944422513971ce42b3a0bd3371d7
Hawin Caraballo <hawin.caraballo@softwareone.com>
2024-01-21 22:56:41 -0500

    Se agrega Migration proyecto Persistence

---
+++ TestRealEstate.Infraestructure.Persistence/Migrations/20240122035547_GenerateTable.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test.RealEstate.Infraestructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class GenerateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owner",
+                columns: table => new
+                {
+                    IdOwner = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Photo = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Birthday = table.Column<DateOnly>(type: "date", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owner", x => x.IdOwner);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Property",
+                columns: table => new
+                {
+                    IdProperty = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    CodeInternal = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: true),
+                    IdOwner = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Property", x => x.IdProperty);
+                    table.ForeignKey(
+                        name: "FK_Property_Owner",
+                        column: x => x.IdOwner,
+                        principalTable: "Owner",
+                        principalColumn: "IdOwner");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyImage",
+                columns: table => new
+                {
+                    IdPropertyImage = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdProperty = table.Column<int>(type: "int", nullable: false),
+                    File = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Enabled = table.Column<bool>(type: "bit", nullable: false),
+                    FileName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    CreatedBy = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyImage", x => x.IdPropertyImage);
+                    table.ForeignKey(
+                        name: "FK_PropertyImage_Property",
+                        column: x => x.IdProperty,
+                        principalTable: "Property",
+                        principalColumn: "IdProperty");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyTrace",
+                columns: table => new
+                {
+                    IdPropertyTrace = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateSale = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Value = table.Column<double>(type: "float", nullable: false),
+                    Tax = table.Column<double>(type: "float", nullable: false),
+                    IdProperty = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyTrace", x => x.IdPropertyTrace);
+                    table.ForeignKey(
+                        name: "FK_PropertyTrace_Property",
+                        column: x => x.IdProperty,
+                        principalTable: "Property",
+                        principalColumn: "IdProperty");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Property_IdOwner",
+                table: "Property",
+                column: "IdOwner");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyImage_IdProperty",
+                table: "PropertyImage",
+                column: "IdProperty");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyTrace_IdProperty",
+                table: "PropertyTrace",
+                column: "IdProperty");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PropertyImage");
+
+            migrationBuilder.DropTable(
+                name: "PropertyTrace");
+
+            migrationBuilder.DropTable(
+                name: "Property");
+
+            migrationBuilder.DropTable(
+                name: "Owner");
+        }
+    }
+}
